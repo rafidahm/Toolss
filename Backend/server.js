943cc0590b1dac9f52d48d9d901fbe22eead0bc8
Rafid <forhadalam228@gmail.com>
2026-01-22 12:21:19 +0600

    DB logic updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+const connectDB = require('./db');
+connectDB(); 
 
 app.get('/', (req, res) => {
     res.send('Backend is running!');
